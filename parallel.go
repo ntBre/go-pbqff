eeba990b248354687b71ff19cf3ea9d26af418e4
Brent Westbrook <brentrwestbrook@gmail.com>
2021-05-24 11:36:59 -0500

    Refactor CheckProg to skip expensive strings.Map and split directly

--- parallel.go
+++ parallel.go
@@ -24,20 +24,15 @@ func CheckProg(cmdfile string) bool {
 	ext := ".prog"
 	progfile := cmdfile + ext
 	logbytes, _ := ioutil.ReadFile(progfile)
-	runes := strings.Map(func(r rune) rune {
-		switch r {
-		case 0x0D: // ^M dos line ending
-			return '\n'
-		default:
-			return r
-		}
-	}, string(logbytes))
-	lines := strings.Split(runes, "\n")
+	lines := strings.Split(string(logbytes), "\x0D")
 	var curjobs, maxjobs int
 	for _, line := range lines {
+		// after splitting on ^M, all of the header
+		// information is on the same "line" so take the 14th
+		// item
 		if strings.Contains(line, "1:local") {
 			fields := strings.Fields(line)
-			maxjobs, _ = strconv.Atoi(fields[len(fields)-1])
+			maxjobs, _ = strconv.Atoi(fields[13])
 		} else if strings.Contains(line, "local:") {
 			// TODO becomes more complicated if maxjobs > 9
 			// need regexp probably to match whole number
