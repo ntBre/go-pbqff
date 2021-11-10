19b6372100124ddd4dfeb1db046d1f090a726c34
Brent Westbrook <brentrwestbrook@gmail.com>
2021-11-10 12:10:37 -0600

    finish fourth derivatives for one plane of symmetry
    this reduces the required number of points for the Cartesian QFF by
    about 32%, at least for the water test case

--- molpro_test.go
+++ molpro_test.go
@@ -736,7 +736,7 @@ func TestBuildCartPoints(t *testing.T) {
 		fc2, fc3, fc4 = t2, t3, t4
 	}()
 	fc2, fc3, fc4 = *new([]CountFloat), *new([]CountFloat), *new([]CountFloat)
-	want := 6041
+	want := 5145
 	mp := new(Molpro)
 	dir := t.TempDir()
 	queue := TestQueue{
