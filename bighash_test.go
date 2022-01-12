d6c0c50e5f9f77fc56db0b8ecf72cf8f4cdcf6a0
Brent Westbrook <brentrwestbrook@gmail.com>
2022-01-12 15:10:42 -0600

    plan new normalization scheme

--- bighash_test.go
+++ bighash_test.go
@@ -1,91 +1,84 @@
 package main
 
-import (
-	"fmt"
-	"testing"
+// func TestNormalize(t *testing.T) {
+// 	// mol := symm.Molecule{
+// 	// 	Atoms: []symm.Atom{
+// 	// 		{"H", []float64{0.9372476886, 0.3147300674, 0.0000000000}},
+// 	// 		{"N", []float64{-0.0000081497, -0.0679517874, 0.0000000000}},
+// 	// 		{"H", []float64{-0.4685672185, 0.3147763140, -0.8117053085}},
+// 	// 		{"H", []float64{-0.4685672185, 0.3147763140, 0.8117053085}},
+// 	// 	},
+// 	// 	Axes:   nil,
+// 	// 	Planes: []symm.Plane{symm.XY},
+// 	// 	Group:  symm.Cs,
+// 	// }
+// 	// got := Normalize(mol, []float64{
+// 	// 	0.9372476886, 0.3147300674, 0.0000000000,
+// 	// 	-0.0000081497, -0.0679517874, 0.0000000000,
+// 	// 	// step this coord by -1
+// 	// 	-1.4685672185, 0.3147763140, -0.8117053085,
+// 	// 	-0.4685672185, 0.3147763140, 0.8117053085,
+// 	// })
+// 	want := [][]float64{
+// 		{
+// 			0.9372476886, 0.3147300674, 0.0000000000,
+// 			-0.0000081497, -0.0679517874, 0.0000000000,
+// 			-0.4685672185, 0.3147763140, -0.8117053085,
+// 			// now this coord is stepped by -1
+// 			-1.4685672185, 0.3147763140, 0.8117053085,
+// 		},
+// 		{
+// 			0.9372476886, 0.3147300674, 0.0000000000,
+// 			-0.0000081497, -0.0679517874, 0.0000000000,
+// 			-1.4685672185, 0.3147763140, -0.8117053085,
+// 			-0.4685672185, 0.3147763140, 0.8117053085,
+// 		},
+// 	}
+// 	// it should give me back what I gave it, plus stepping the
+// 	// atom on the other side by the same amount
+// 	fmt.Println("got")
+// 	for _, row := range got {
+// 		for i := 0; i < len(row)/3; i++ {
+// 			fmt.Printf("%15.10f\n", row[3*i:3*i+3])
+// 		}
+// 		fmt.Println("---")
+// 	}
+// 	fmt.Println("want")
+// 	for _, row := range want {
+// 		for i := 0; i < len(row)/3; i++ {
+// 			fmt.Printf("%15.10f\n", row[3*i:3*i+3])
+// 		}
+// 		fmt.Println("---")
+// 	}
 
-	symm "github.com/ntBre/chemutils/symmetry"
-)
+// 	// if there is an atom where the reflection puts this atom,
+// 	// this step is equivalent to the same step on that atom
 
-func TestNormalize(t *testing.T) {
-	mol := symm.Molecule{
-		Atoms: []symm.Atom{
-			{"H", []float64{0.9372476886, 0.3147300674, 0.0000000000}},
-			{"N", []float64{-0.0000081497, -0.0679517874, 0.0000000000}},
-			{"H", []float64{-0.4685672185, 0.3147763140, -0.8117053085}},
-			{"H", []float64{-0.4685672185, 0.3147763140, 0.8117053085}},
-		},
-		Axes:   nil,
-		Planes: []symm.Plane{symm.XY},
-		Group:  symm.Cs,
-	}
-	got := Normalize(mol, []float64{
-		0.9372476886, 0.3147300674, 0.0000000000,
-		-0.0000081497, -0.0679517874, 0.0000000000,
-		// step this coord by -1
-		-1.4685672185, 0.3147763140, -0.8117053085,
-		-0.4685672185, 0.3147763140, 0.8117053085,
-	})
-	want := [][]float64{
-		{
-			0.9372476886, 0.3147300674, 0.0000000000,
-			-0.0000081497, -0.0679517874, 0.0000000000,
-			-0.4685672185, 0.3147763140, -0.8117053085,
-			// now this coord is stepped by -1
-			-1.4685672185, 0.3147763140, 0.8117053085,
-		},
-		{
-			0.9372476886, 0.3147300674, 0.0000000000,
-			-0.0000081497, -0.0679517874, 0.0000000000,
-			-1.4685672185, 0.3147763140, -0.8117053085,
-			-0.4685672185, 0.3147763140, 0.8117053085,
-		},
-	}
-	// it should give me back what I gave it, plus stepping the
-	// atom on the other side by the same amount
-	fmt.Println("got")
-	for _, row := range got {
-		for i := 0; i < len(row)/3; i++ {
-			fmt.Printf("%15.10f\n", row[3*i:3*i+3])
-		}
-		fmt.Println("---")
-	}
-	fmt.Println("want")
-	for _, row := range want {
-		for i := 0; i < len(row)/3; i++ {
-			fmt.Printf("%15.10f\n", row[3*i:3*i+3])
-		}
-		fmt.Println("---")
-	}
+// 	// maybe I need to break it up into one atom at a time
 
-	// if there is an atom where the reflection puts this atom,
-	// this step is equivalent to the same step on that atom
+// 	// if part of the step affects this atom, do the symmetry
+// 	// operation, check if there is an atom in the new location in
+// 	// the original geometry. if there is, this step is equivalent
+// 	// to stepping that atom
 
-	// maybe I need to break it up into one atom at a time
+// 	// but how does that extend to combinations of steps?
 
-	// if part of the step affects this atom, do the symmetry
-	// operation, check if there is an atom in the new location in
-	// the original geometry. if there is, this step is equivalent
-	// to stepping that atom
+// 	// I want a generic transformation, not implementing each
+// 	// n-atom transformation separately, that's what I had before
+// }
 
-	// but how does that extend to combinations of steps?
+// // when I perform the symmetry operation *I* know that the result is
+// // equivalent to the original by symmetry. I just need to put it in a
+// // form that the computer will recognize
 
-	// I want a generic transformation, not implementing each
-	// n-atom transformation separately, that's what I had before
-}
+// // just swap the pairs of atoms affected, that works for this case at
+// // least
 
-// when I perform the symmetry operation *I* know that the result is
-// equivalent to the original by symmetry. I just need to put it in a
-// form that the computer will recognize
+// // so I do need something like IsSame that returns the new order of
+// // the atoms, and I need to do that operation on the original atoms,
+// // not the stepped ones
 
-// just swap the pairs of atoms affected, that works for this case at
-// least
-
-// so I do need something like IsSame that returns the new order of
-// the atoms, and I need to do that operation on the original atoms,
-// not the stepped ones
-
-// the solution for my only test case currently is to swap the rows,
-// but I don't think that is generalizable. it's also not clear how to
-// identify the rows to be swapped besides just the ones different
-// from before
+// // the solution for my only test case currently is to swap the rows,
+// // but I don't think that is generalizable. it's also not clear how to
+// // identify the rows to be swapped besides just the ones different
+// // from before
