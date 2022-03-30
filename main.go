ntBre/go-pbqff

commit 735e6d9d637d98af0e03bb6491425967661b5547

track down very subtle CartProd bug
there was some kind of issue with x getting captured in the outer loop. the
first element of tmp was changing between the start and end of the inner loop,
and this only popped up for more than 3 elements in the parts. I think for less
than that it must have been reallocating anyway

main.go
@@ -536,7 +536,6 @@ func main() {
 	other3, other4 := initArrays(natoms)
 
 	var forces [][]int
-	fmt.Println(forces)
 	if SIC {
 		intder.WritePts("pts/intder.in")
 		RunIntder("pts/intder")
@@ -606,7 +605,7 @@ func main() {
 			}
 		}
 		exps := mat.NewDense(len(forces[0]), len(forces), nforces)
-		steps := DispToStep(Disps(forces))
+		steps := DispToStep(Disps(forces, false))
 		stepdat := make([]float64, 0)
 		for _, step := range steps {
 			stepdat = append(stepdat,

embed/taylor.py
@@ -162,7 +162,6 @@ def eqCheck(e, eqchecks):
         print("# Equivalence checking is enabled.")
     for k in eqchecks.keys():
         for r in eqchecks[k]:
-            print(f"e: {e}, r: {r}, k: {k}")
             start = r[0]
             end = r[1]
             sub = e[start - 1 : end]
@@ -242,13 +241,16 @@ def displacements(e):
         prods.append(tmp)
     if args.verbose and not args.silent:
         print("prods: ", prods)
+    print(prods)
     newrows = map(list, itertools.product(*prods))
+    print(f"newrows: {list(newrows)}")
     if args.verbose and not args.silent:
         print("# Displacement combinations: " + str(list(newrows)))
-    # Build each new displacement row by copying the force constant row then overwriting
-    #   the non-zero values with the calculated values
+    # Build each new displacement row by copying the force constant row then
+    #   overwriting the non-zero values with the calculated values
     result = []
     for row in newrows:
+        print(f"row: {row}")
         # make a copy of 'e'
         r = list(e)
         for i, index in enumerate(indexes):
@@ -256,7 +258,7 @@ def displacements(e):
         result.append(r)
     if args.verbose and not args.silent:
         print("# Calculated a total of " + str(len(result)) + " displacements")
-        print("# Displacement list: " + str(result))
+        print(f"# Displacement list: {result}")
     return result
 
 

full_test.go
@@ -154,7 +154,7 @@ func TestCart(t *testing.T) {
 		},
 	}
 	*overwrite = true
-	for _, test := range tests[0:] {
+	for _, test := range tests[0:1] {
 		*nosym = test.nosym
 		Conf = ParseInfile(test.infile).ToConfig()
 		Global.Submitted = 0
@@ -183,7 +183,7 @@ func TestCart(t *testing.T) {
 			}
 		}
 		exps := mat.NewDense(len(forces[0]), len(forces), nforces)
-		steps := DispToStep(Disps(forces))
+		steps := DispToStep(Disps(forces,false))
 		stepdat := make([]float64, 0)
 		for _, step := range steps {
 			stepdat = append(stepdat,

program.go
@@ -178,7 +178,7 @@ func BuildCartPoints(prog Program, q Queue, dir string, names []string,
 	dir = filepath.Join(prog.GetDir(), dir)
 	ncoords := len(coords)
 	forces = newTaylor(Conf.Deriv+1, ncoords, nil, nil)
-	disps := Disps(forces)
+	disps := Disps(forces, false)
 	disps = DispToStep(disps)
 	calcs := make([]Calc, len(disps))
 	for geom, disp := range disps {

taylor.go
@@ -2,8 +2,11 @@
 package main
 
 import (
+	"bufio"
 	_ "embed"
 	"fmt"
+	"io"
+	"os"
 	"strings"
 )
 
@@ -133,9 +136,29 @@ func NextRow(row []int, n, m int) int {
 	return index
 }
 
+func MakeDisps(w io.Writer, disps [][]int) {
+	for _, row := range disps {
+		lrow := len(row) - 1
+		for i, d := range row {
+			fmt.Fprintf(w, "%d", d)
+			if i < lrow {
+				fmt.Fprint(w, ",")
+			}
+		}
+		fmt.Fprint(w, "\n")
+	}
+}
+
+func WriteDisps(filename string, disps [][]int) {
+	f, _ := os.Create(filename)
+	defer f.Close()
+	bf := bufio.NewWriter(f)
+	defer bf.Flush()
+	MakeDisps(bf, disps)
+}
+
 // Disps generates the displacments corresponding to fcs
-func Disps(fcs [][]int) (disps [][]int) {
-	var idx int = 1
+func Disps(fcs [][]int, dups bool) (disps [][]int) {
 	for _, row := range fcs {
 		indices := make([]int, 0)
 		values := make([]int, 0)
@@ -164,9 +187,12 @@ func Disps(fcs [][]int) (disps [][]int) {
 			for i, index := range indices {
 				r[index] = nrow[i]
 			}
+			// fmt.Printf("# Displacement list: %d\n", r)
 			disps = append(disps, r)
-			idx++
 		}
+	}
+	if dups {
+		return disps
 	}
 	return Deduplicate(disps)
 }
@@ -180,7 +206,11 @@ func CartProd(pools [][]int) [][]int {
 		tmp := make([][]int, 0)
 		for _, x := range result {
 			for _, y := range pool {
-				tmp = append(tmp, append(x, y))
+				l := len(x)+1
+				a := make([]int, l)
+				copy(a, x)
+				a[l-1] = y
+				tmp = append(tmp, a)
 			}
 		}
 		result = tmp

taylor_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"reflect"
 	"strconv"
@@ -20,7 +21,8 @@ import (
 // 	Taylor([]string{"H", "O", "H"}, intder)
 // }
 
-func loadForces(filename string) (ret [][]int) {
+// load a 2-dimensional int list with the columns separated by commas
+func loadInt2D(filename string) (ret [][]int) {
 	f, _ := os.Open(filename)
 	scanner := bufio.NewScanner(f)
 	var fields []string
@@ -61,7 +63,8 @@ func TestNewTaylor(t *testing.T) {
 			},
 		},
 		{
-			m: 5, n: 3, mods: [][]int{
+			m: 5, n: 3,
+			mods: [][]int{
 				{3, 3},
 				{0, 0},
 				{0, 0},
@@ -94,7 +97,7 @@ func TestNewTaylor(t *testing.T) {
 				{8, 8},
 				{9, 9},
 			},
-			want: loadForces("testfiles/load/force.txt"),
+			want: loadInt2D("testfiles/load/force.txt"),
 		},
 	}
 	for _, test := range tests {
@@ -122,140 +125,72 @@ func TestNextRow(t *testing.T) {
 }
 
 func TestDisps(t *testing.T) {
-	got := Disps(newTaylor(5, 3, nil, nil))
-	want := [][]int{
-		{0, 0, 0},
-		{0, 0, -1},
-		{0, 0, 1},
-		{0, 0, -2},
-		{0, 0, 2},
-		{0, 0, -3},
-		{0, 0, 3},
-		{0, 0, -4},
-		{0, 0, 4},
-		{0, -1, 0},
-		{0, 1, 0},
-		{0, -1, -1},
-		{0, -1, 1},
-		{0, 1, -1},
-		{0, 1, 1},
-		{0, -1, -2},
-		{0, -1, 2},
-		{0, 1, -2},
-		{0, 1, 2},
-		{0, -1, -3},
-		{0, -1, 3},
-		{0, 1, -3},
-		{0, 1, 3},
-		{0, -2, 0},
-		{0, 2, 0},
-		{0, -2, -1},
-		{0, -2, 1},
-		{0, 2, -1},
-		{0, 2, 1},
-		{0, -2, -2},
-		{0, -2, 2},
-		{0, 2, -2},
-		{0, 2, 2},
-		{0, -3, 0},
-		{0, 3, 0},
-		{0, -3, -1},
-		{0, -3, 1},
-		{0, 3, -1},
-		{0, 3, 1},
-		{0, -4, 0},
-		{0, 4, 0},
-		{-1, 0, 0},
-		{1, 0, 0},
-		{-1, 0, -1},
-		{-1, 0, 1},
-		{1, 0, -1},
-		{1, 0, 1},
-		{-1, 0, -2},
-		{-1, 0, 2},
-		{1, 0, -2},
-		{1, 0, 2},
-		{-1, 0, -3},
-		{-1, 0, 3},
-		{1, 0, -3},
-		{1, 0, 3},
-		{-1, -1, 0},
-		{-1, 1, 0},
-		{1, -1, 0},
-		{1, 1, 0},
-		{-1, -1, -1},
-		{-1, -1, 1},
-		{-1, 1, -1},
-		{-1, 1, 1},
-		{1, -1, -1},
-		{1, -1, 1},
-		{1, 1, -1},
-		{1, 1, 1},
-		{-1, -1, -2},
-		{-1, -1, 2},
-		{-1, 1, -2},
-		{-1, 1, 2},
-		{1, -1, -2},
-		{1, -1, 2},
-		{1, 1, -2},
-		{1, 1, 2},
-		{-1, -2, 0},
-		{-1, 2, 0},
-		{1, -2, 0},
-		{1, 2, 0},
-		{-1, -2, -1},
-		{-1, -2, 1},
-		{-1, 2, -1},
-		{-1, 2, 1},
-		{1, -2, -1},
-		{1, -2, 1},
-		{1, 2, -1},
-		{1, 2, 1},
-		{-1, -3, 0},
-		{-1, 3, 0},
-		{1, -3, 0},
-		{1, 3, 0},
-		{-2, 0, 0},
-		{2, 0, 0},
-		{-2, 0, -1},
-		{-2, 0, 1},
-		{2, 0, -1},
-		{2, 0, 1},
-		{-2, 0, -2},
-		{-2, 0, 2},
-		{2, 0, -2},
-		{2, 0, 2},
-		{-2, -1, 0},
-		{-2, 1, 0},
-		{2, -1, 0},
-		{2, 1, 0},
-		{-2, -1, -1},
-		{-2, -1, 1},
-		{-2, 1, -1},
-		{-2, 1, 1},
-		{2, -1, -1},
-		{2, -1, 1},
-		{2, 1, -1},
-		{2, 1, 1},
-		{-2, -2, 0},
-		{-2, 2, 0},
-		{2, -2, 0},
-		{2, 2, 0},
-		{-3, 0, 0},
-		{3, 0, 0},
-		{-3, 0, -1},
-		{-3, 0, 1},
-		{3, 0, -1},
-		{3, 0, 1},
-		{-3, -1, 0},
-		{-3, 1, 0},
-		{3, -1, 0},
-		{3, 1, 0},
-		{-4, 0, 0},
-		{4, 0, 0},
+	tests := []struct {
+		mods [][]int
+		eqs  [][]int
+		want [][]int
+		m    int
+		n    int
+		dups bool
+	}{
+		{
+			m: 5, n: 3,
+			mods: nil, eqs: nil,
+			want: loadInt2D("testfiles/load/dispu.h2o.txt"),
+		},
+		{
+			m: 5, n: 3,
+			mods: [][]int{
+				{3, 3},
+				{0, 0},
+				{0, 0},
+			},
+			eqs: [][]int{
+				{3, 3},
+				{0, 0},
+				{0, 0},
+			},
+			want: loadInt2D("testfiles/load/dispu.h2o.mod.txt"),
+		},
+		{
+			m: 5, n: 9,
+			want: loadInt2D("testfiles/load/dispu.c3h2.txt"),
+		},
+		{
+			m: 5, n: 9,
+			want: loadInt2D("testfiles/load/disp.c3h2.txt"),
+			dups: true,
+		},
+		{
+			m: 5, n: 9,
+			mods: [][]int{
+				{5, 7},
+				{8, 8},
+				{9, 9},
+			},
+			eqs: [][]int{
+				{5, 7},
+				{8, 8},
+				{9, 9},
+			},
+			want: loadInt2D("testfiles/load/dispu.c3h2.mod.txt"),
+		},
 	}
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v, wanted %v\n", got, want)
+	for _, test := range tests {
+		got := Disps(
+			newTaylor(test.m, test.n, test.mods, test.eqs),
+			test.dups,
+		)
+		if !reflect.DeepEqual(got, test.want) {
+			gf, _ := os.CreateTemp("", "got.*")
+			wf, _ := os.CreateTemp("", "want.*")
+			MakeDisps(gf, got)
+			MakeDisps(wf, test.want)
+			t.Errorf("got %v, wanted %v\n(diff %q %q)\n",
+				len(got), len(test.want),
+				gf.Name(), wf.Name(),
+			)
+		}
 	}
 }
 
@@ -301,11 +236,50 @@ func TestCartProd(t *testing.T) {
 				{1, 1, 1},
 			},
 		},
+		{
+			msg: "problem arising in disps",
+			inp: [][]int{
+				{-1, 1},
+				{-1, 1},
+				{-1, 1},
+				{-1, 1},
+			},
+			want: [][]int{
+				{-1, -1, -1, -1},
+				{-1, -1, -1, 1},
+				{-1, -1, 1, -1},
+				{-1, -1, 1, 1},
+				{-1, 1, -1, -1},
+				{-1, 1, -1, 1},
+				{-1, 1, 1, -1},
+				{-1, 1, 1, 1},
+				{1, -1, -1, -1},
+				{1, -1, -1, 1},
+				{1, -1, 1, -1},
+				{1, -1, 1, 1},
+				{1, 1, -1, -1},
+				{1, 1, -1, 1},
+				{1, 1, 1, -1},
+				{1, 1, 1, 1},
+			},
+		},
 	}
 	for _, test := range tests {
 		got := CartProd(test.inp)
 		if !reflect.DeepEqual(got, test.want) {
-			t.Errorf("got %v, wanted %v\n", got, test.want)
+			fmt.Printf("got len = %d, wanted %d\n",
+				len(got), len(test.want),
+			)
+			t.Errorf("(%s): got %v, wanted %v\n",
+				test.msg, got, test.want,
+			)
+			for i, g := range got {
+				if !reflect.DeepEqual(g, test.want[i]) {
+					fmt.Printf("%d %d %d\n",
+						i, g, test.want[i],
+					)
+				}
+			}
 		}
 	}
 }

commit 868e37407f7c2d108bfcc1906a9077a43b569379

fix constant resubmit bug introduced in 735e6d9

main.go
@@ -188,7 +188,7 @@ func Drain(prog Program, q Queue, ncoords int, E0 float64,
 				realTime += t
 				heap.Add(job.Name)
 				// job has not been resubmitted && there is an error
-			} else if !job.noRun &&
+			} else if !job.noRun && job.Resub == nil &&
 				(err == ErrFinishedButNoEnergy || err ==
 					ErrFileContainsError || err ==
 					ErrBlankOutput || (err ==